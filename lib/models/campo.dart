03c344920d70c21fd6543ae0972c5df5aada2966
Carlos <als04545@gmail.com>
2020-04-11 09:35:07 -0300

    Start commit

---
+++ lib/models/campo.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/foundation.dart';
+import 'explosao_exception.dart';
+
+class Campo {
+  final int linha;
+  final int coluna;
+  final List<Campo> vizinhos = [];
+
+  bool _aberto = false;
+  bool _marcado = false;
+  bool _minado = false;
+  bool _explodido = false;
+
+  Campo({
+    @required this.linha,
+    @required this.coluna,
+  });
+
+  // (2,3) (2,4) (2,5)
+  // (3,3) (3,4) (3,5)
+  // (4,3) (4,4) (4,5)
+  void adicionarVizinho(Campo vizinho) {
+    final deltaLinha = (linha - vizinho.linha).abs();
+    final deltaColuna = (coluna - vizinho.coluna).abs();
+
+    if (deltaLinha == 0 && deltaColuna == 0) {
+      return;
+    }
+
+    if (deltaLinha <= 1 && deltaColuna <= 1) {
+      vizinhos.add(vizinho);
+    }
+  }
+
+  void abrir() {
+    if (_aberto) {
+      return;
+    }
+
+    _aberto = true;
+
+    if (_minado) {
+      _explodido = true;
+      throw ExplosaoException();
+    }
+
+    if (vizinhancaSegura) {
+      vizinhos.forEach((v) => v.abrir());
+    }
+  }
+
+  void minar() {
+    _minado = true;
+  }
+
+  void alternarMarcacao() {
+    _marcado = !_marcado;
+  }
+
+  void reiniciar() {
+    _aberto = false;
+    _marcado = false;
+    _minado = false;
+    _explodido = false;
+  }
+
+  void revelaBomba() {
+    if (_minado) {
+      _aberto = true;
+    }
+  }
+
+  bool get minado {
+    return _minado;
+  }
+
+  bool get explodido {
+    return _explodido;
+  }
+
+  bool get aberto {
+    return _aberto;
+  }
+
+  bool get marcado {
+    return _marcado;
+  }
+
+  bool get resolvido{
+    bool minadoEMarcado = minado && marcado;
+    bool seguroEAberto = !minado && aberto;
+    return minadoEMarcado || seguroEAberto;
+  }
+
+  bool get vizinhancaSegura {
+    return vizinhos.every((v) => !v._minado);
+  }
+
+  int get qtdeMinasNaVizinhanca{
+    return vizinhos.where((v) => v.minado).length;
+  }
+}
